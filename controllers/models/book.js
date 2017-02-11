884c52e857a6558396d2ddcde8495e44e0b320e0
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-11 01:52:10 +0000

    Create the book mongoose Schema model

---
+++ controllers/models/book.js
@@ -0,0 +1,28 @@
+'use strict';
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Boook schema definition
+
+const BookSchema = new Schema({
+    title: {type: String, required: true},
+    author: {type: String, required: true},
+    year: {type: Number, required: true},
+    pages: {type: Number, required: true},
+    createdAt: {type: Date, default: Date.now}
+}, {
+    versionKey: false
+});
+
+// Sets the createdAt parameter equal to the current time
+
+BookSchema.pre('save', next => {
+    const now = new Date();
+    if(!this.createdAt) {
+        this.createdAt = now;
+    }
+    next();
+});
+
+// Exports the BookSchema for use elsewhere
+module.exports = mongoose.model('book', BookSchema);
