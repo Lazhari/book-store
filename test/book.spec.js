4d6827058519977f6401b1ef427f4e03bc676762
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-02-11 21:52:22 +0000

    Fixing issue on delete book

--- test/book.spec.js
+++ test/book.spec.js
@@ -220,15 +220,14 @@ describe('Books', () => {
         });
         it('it should return an error if the book ID is not valid', (done) => {
             chai.request(server)
-                .put('/book/90')
-                .send({year: 2009})
+                .delete('/book/90')
                 .end((err, res) => {
                     res.should.have.status(400);
                     res.body.should.be.a('object');
                     res.body.should.have.property('message');
                     res.body.should.have.property('name');
                     res.body.should.have.property('kind').eql('ObjectId');
-                    res.body.value.should.have.property('_id').eql('90');
+                    res.body.should.have.property('value').eql('90');
                     res.body.should.have.property('path').eql('_id');
                     done();
                 });
@@ -236,8 +235,7 @@ describe('Books', () => {
 
         it('it should return not found if the book ID don\'t exist', (done) => {
             chai.request(server)
-                .put('/book/589e02e559f531603fe40322')
-                .send({year: 2010})
+                .delete('/book/589e02e559f531603fe40322')
                 .end((err, res) => {
                     res.should.have.status(404);
                     res.body.should.be.a('object');
